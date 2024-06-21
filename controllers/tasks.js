f908ef7df293a82cb841395e47ba007f5149bf78
Saksham <skshm.090898@gmail.com>
2024-06-21 17:32:01 +0530

    updated create and get routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,33 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-    res.send('all items from controller');
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (err) {
+        res.status(500).json({ msg: err });
+    }
 }
 
 const createTask = async (req, res) => {
-    
-    try{
+    try {
         const task = await Task.create(req.body);
         res.status(201).json({ task });
-    }catch(err){
-        res.status(500).json({msg: err});
+    } catch (err) {
+        res.status(500).json({ msg: err });
     }
-
 }
-const getTask = (req, res) => {
-    res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOne({_id:taskID});
+        if(!task){
+            return res.status(400).json({msg:`No task with id: ${taskID}`});
+        }
+        res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 }
 const updateTask = (req, res) => {
     res.send('updated task');
