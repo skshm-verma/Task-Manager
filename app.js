421fc0610265be17aba791e9a508c27e8b22188c
Saksham <skshm.090898@gmail.com>
2024-06-21 19:23:38 +0530

    added the frontend part and still dealing with error handlers

--- app.js
+++ app.js
@@ -3,25 +3,30 @@ const app = express(); //we initialize and invoke it(express)
 const tasks = require('./routes/tasks');
 const connectDb = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+
 
 //middleware
-app.use(express.json())
+app.use(express.static('./public'));
+app.use(express.json());
 
 //routes
-app.get('/health',(req,res)=> {
-    res.send('Task Manager App');
-})
+// app.get('/health',(req,res)=> {
+//     res.send('Task Manager App');      Not required to check anymore
+// })
 
 app.use('/api/v1/tasks',tasks);
 
-const port = 9000
+app.use(notFound);
+
+const port = 9000;
 
 const start = async () => {
     try{
-       await connectDb(process.env.MONGO_URI)
+       await connectDb(process.env.MONGO_URI);
        app.listen(port, console.log(`Server is listening on port ${port}`));
     }catch(error){
-        console.log(error)
+        console.log(error);
     }
 }
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,40 +1,60 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');  // way to avoid try catch redundancy in the controllers
 
-const getAllTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.status(200).json({ tasks });
-    } catch (err) {
-        res.status(500).json({ msg: err });
-    }
-}
 
-const createTask = async (req, res) => {
-    try {
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+})
+
+
+const createTask = asyncWrapper( async (req, res) => {
         const task = await Task.create(req.body);
         res.status(201).json({ task });
+})
+
+
+/* Code written when using try catch block with the controllers */
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOne({ _id: taskID });
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        }
+        res.status(200).json({ task });
     } catch (err) {
         res.status(500).json({ msg: err });
     }
 }
-const getTask = async (req, res) => {
-    try {
-        const {id: taskID} = req.params;
-        const task = await Task.findOne({_id:taskID});
-        if(!task){
-            return res.status(400).json({msg:`No task with id: ${taskID}`});
+
+
+const deleteTask = asyncWrapper( async (req, res) => {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndDelete({ _id: taskID });
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
         }
-        res.status(200).json({task});
-    } catch (error) {
-        res.status(500).json({ msg: error });
-    }
-}
-const updateTask = (req, res) => {
-    res.send('updated task');
-}
-const deleteTask = (req, res) => {
-    res.send('deleted task');
-}
+        //various ways to send response back;
+        // res.status(200).json({task});
+        // res.status(200).send();
+        res.status(200).json({ task: null, status: 'success' });
+})
+
+const updateTask = asyncWrapper( async (req, res) => {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndUpdate(
+            { _id: taskID },
+            req.body,
+            { new: true, runValidators: true } // the new key will always send the updated data and runValidators runs 
+            // all the validations written in the backend model schema
+            // learn about 'overwrite key' as well can be used with put http method!!
+        )
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        }
+        res.status(200).json({ task, status: 'Updated Successfully' });
+})
 
 module.exports = {
     getAllTasks,
